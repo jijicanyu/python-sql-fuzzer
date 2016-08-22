5a9ba44b5c30696bfcf7cce223d12d030a31183d
thinkycx <b13040325@njupt.edu.cn>
2016-08-21 22:19:03 -0700

    out put example

--- mysqldb.py
+++ mysqldb.py
@@ -6,12 +6,15 @@ USER = 'root'
 PASSWD = 'root'
 DB = 'test'
 
-
-raw = [chr(i) for i in range(32, 49)]
+# del ; and # incase of dis
+raw = [chr(i) for i in range(32, 49)]  #49
 raw.extend([chr(i) for i in range(58, 66)])
 raw.extend([chr(i) for i in range(91, 98)])
-raw.extend([chr(i) for i in range(123, 127)])
-raw.extend(['N', 'e'])
+raw.extend([chr(i) for i in range(123, 128)])
+raw.extend([':','N', 'e'])
+raw_del = ['#',';']
+for i in raw_del:
+    raw.remove(i)
 print raw
 conn = MySQLdb.connect(host=HOST, user=USER, passwd=PASSWD, db=DB)
 
@@ -22,13 +25,18 @@ def three():
         for j in range(len(raw)):
             for k in range(len(raw)):
                 cursor = conn.cursor()
-                SQL_QUERY = 'select 1[1]union select 1 from good where 1 limit 1;'
+                SQL_QUERY = 'select schema_name from information_schema.schemata where[1]schema_name=\'information_schema\''
+                # SQL_QUERY = SQL_QUERY.replace(r"'","\'")
                 payload = raw[i] + raw[j] + raw[k]
                 SQL_QUERY = SQL_QUERY.replace('[1]', payload)
                 try:
                     cursor.execute(SQL_QUERY)
-                    print SQL_QUERY
-                    count += 1
+                    #if '#' in (raw[i],raw[j],raw[k]):
+                    if 'information_schema' in cursor.fetchone()[0]:
+                        with open('output','a') as f:
+                            f.write(SQL_QUERY+'\n')
+                        count += 1
+
                     cursor.close()  # must close
                 except:
                     pass
